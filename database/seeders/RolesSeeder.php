8f9976cbe96475031e10d65fcdbd4ac55b50cae6
Tatan <tatangamess@gmail.com>
2026-07-16 15:22:58 -0600

    listo para revisiones finales

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -18,18 +18,6 @@ class RolesSeeder extends Seeder
         // Administrador
         $roleAdmin = Role::create(['name' => 'admin', 'guard_name' => 'admin']);
 
-        // Archivo
-        $roleArchivo = Role::create(['name' => 'archivo', 'guard_name' => 'admin']);
-
-        // Enfermeria
-        $roleEnfermeria = Role::create(['name' => 'enfermeria', 'guard_name' => 'admin']);
-
-        // Doctora
-        $roleDoctora = Role::create(['name' => 'doctora', 'guard_name' => 'admin']);
-
-        // Farmacia
-        $roleFarmacia = Role::create(['name' => 'farmacia', 'guard_name' => 'admin']);
-
 
         // solo para administrador
         Permission::create(['name' => 'sidebar.roles.y.permisos', 'description' => 'sidebar seccion roles y permisos'])->syncRoles($roleAdmin);
--- database/seeders/UsuariosSeeder.php
+++ database/seeders/UsuariosSeeder.php
@@ -21,42 +21,6 @@ class UsuariosSeeder extends Seeder
             'activo' => 1,
         ])->assignRole('admin');
 
-        Usuario::create([
-            'nombre' => 'Giovany',
-            'usuario' => 'giovany',
-            'password' => bcrypt('1234'),
-            'activo' => 1,
-        ])->assignRole('admin');
 
-
-        Usuario::create([
-            'nombre' => 'Archivo',
-            'usuario' => 'archivo',
-            'password' => bcrypt('1234'),
-            'activo' => 1,
-        ])->assignRole('archivo');
-
-        Usuario::create([
-            'nombre' => 'Enfermeria',
-            'usuario' => 'enfermeria',
-            'password' => bcrypt('1234'),
-            'activo' => 1,
-        ])->assignRole('enfermeria');
-
-
-        Usuario::create([
-            'nombre' => 'Doctora',
-            'usuario' => 'doctora',
-            'password' => bcrypt('1234'),
-            'activo' => 1,
-        ])->assignRole('doctora');
-
-
-        Usuario::create([
-            'nombre' => 'Farmacia',
-            'usuario' => 'farmacia',
-            'password' => bcrypt('1234'),
-            'activo' => 1,
-        ])->assignRole('farmacia');
     }
 }
