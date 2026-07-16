12e3719433930cf1e371709d2ee743f29bc7646d
Tatan <tatangamess@gmail.com>
2026-07-16 17:01:55 -0600

    listo para revisiones finales

--- app/Http/Controllers/Sistema/ControlController.php
+++ app/Http/Controllers/Sistema/ControlController.php
@@ -17,12 +17,23 @@ class ControlController extends Controller
         }
 
         // Inventario
-        else  if($user->hasRole('inventario')){
-            return redirect()->route('admin.materiales.index');
+        else  if($user->hasRole('archivo')){
+            return redirect()->route('admin.asignaciones.index');
         }
 
-        else  if($user->hasRole('auditora')){
-            return redirect()->route('admin.entrada.reporte.index');
+        // Enfermeria
+        else  if($user->hasRole('enfermeria')){
+            return redirect()->route('admin.asignaciones.index');
+        }
+
+        // Doctora
+        else  if($user->hasRole('doctora')){
+            return redirect()->route('admin.asignaciones.index');
+        }
+
+        // Farmacia
+        else  if($user->hasRole('farmacia')){
+            return redirect()->route('admin.asignaciones.index');
         }
 
         return redirect()->route('no.permisos.index');
